206e8eb42ed882a488b42415c23cf2c5e41c16dc
narisettiashok <nleoashok5555@gmail.com>
2021-04-29 14:45:47 +0530

    Header Bar completed

---
+++ civil-blog-react/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+import "../css/Header.css";
+
+
+const Header = () => <div className="header-Main">  
+                        <div className="logo"> 
+                        <img className="logo-img" src="../images/logo.jpg" /> 
+                        </div>
+                        <div className="nav-items"> 
+                            <a href="#"> Home </a>
+                            <a href="#"> About </a>
+                            <a href="#"> Projects </a>
+                            <a href="#"> Contact </a>
+                        </div>
+                        <div className="login-signup">
+                            <button className="login-signup-btn"> Login/Signup </button>
+                        </div>
+                    </div>
+
+export default Header;
