1d24a8c9d6a050091e62b582a6137ec5b7e1b2a2
Deepak <deepakrbabu83@gmail.com>
2021-05-12 05:40:29 +0530

    Add tailwindcss and its dependencies

--- Field-Construct/src/components/Header.jsx
+++ Field-Construct/src/components/Header.jsx
@@ -3,19 +3,20 @@ import reactDOM from 'react-dom';
 import "../css/Header.css";
 
 
-const Header = () => <div className="header-Main">  
-                        <div className="logo"> 
-                        <img className="logo-img" src="../images/logo.jpg" /> 
-                        </div>
-                        <div className="nav-items"> 
-                            <a href="#"> Home </a>
-                            <a href="#"> About </a>
-                            <a href="#"> Projects </a>
-                            <a href="#"> Contact </a>
-                        </div>
-                        <div className="login-signup">
-                            <button className="login-signup-btn"> Login/Signup </button>
-                        </div>
-                    </div>
+const Header = () =>
+    <div className="header-Main">
+        <div className="logo">
+            <img className="logo-img" src="../images/logo.jpg" />
+        </div>
+        <div className="nav-items">
+            <a href="#" className="text-xs"> Home </a>
+            <a href="#"> About </a>
+            <a href="#"> Projects </a>
+            <a href="#"> Contact </a>
+        </div>
+        <div className="login-signup">
+            <button className="login-signup-btn"> Login/Signup </button>
+        </div>
+    </div>
 
 export default Header;
