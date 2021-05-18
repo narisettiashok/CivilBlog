d764f7de055e7c5f33368060f5a2501f94aa5d12
narisettiashok <nleoashok5555@gmail.com>
2021-05-18 08:02:01 +0530

    Forgot Password page updated

--- src/pages/Auth/ForgotPasswordPage.jsx
+++ src/pages/Auth/ForgotPasswordPage.jsx
@@ -16,9 +16,9 @@ function ForgotPasswordPage() {
                     <form>
                         <div className="form-group">
                             <h1 className="uppercase font-bold text-xl text-center pb-3">
-                                Forgot Password?
+                                Forgot Password
                             </h1>
-                            <p className="text-xs text-center pb-3">
+                            <p className="text-xs text-center pb-10">
                                 Please enter your registered email ID and company name to reset password?
                             </p>
                         </div>
@@ -35,22 +35,10 @@ function ForgotPasswordPage() {
                             />
                         </div>
 
-                        <div className="form-group">
-                            <label className="form-label"
-                                htmlFor="companyName">
-                                Company
-                            </label>
-                            <input type="text"
-                                id="companyName"
-                                className="form-control"
-                                placeholder="Company Name"
-                            />
-                        </div>
-
                         <div className="text-center mt-6">
                             <Link className="bg-blue-800 text-white active:bg-blue-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                                 type="button" to="/auth/reset-password">
-                                Reset Password?
+                                Reset Password
                             </Link>
                         </div>
                     </form>
